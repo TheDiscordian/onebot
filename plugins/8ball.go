c2bf107583f4b7d779e892288af344d723e36cf7
TheDiscordian <therealdiscordian@gmail.com>
2020-05-14 02:31:53 -0400

    made lint happy

--- plugins/8ball.go
+++ plugins/8ball.go
@@ -27,13 +27,13 @@ func eightball(msg onelib.Message, sender onelib.Sender) {
 	if len(text) < 3 {
 		text = fmt.Sprintf("Predicts the future. Usage: %s8ball <y/n question>", onelib.DefaultPrefix)
 	} else {
-		eightball_answers := []string{"As I see it, yes", "It is certain", "It is decidedly so", "Most likely",
+		eightballAnswers := []string{"As I see it, yes", "It is certain", "It is decidedly so", "Most likely",
 			"Outlook good", "Signs point to yes", "Without a doubt", "Yes", "Yes, definitely",
 			"You may rely on it", "Reply hazy, try again", "Ask again later",
 			"Better not tell you now", "Cannot predict now", "Concentrate and ask again",
 			"Don't count on it", "My reply is no", "My sources say no", "Outlook not so good",
 			"Very doubtful"}
-		text = eightball_answers[rand.Intn(len(eightball_answers))]
+		text = eightballAnswers[rand.Intn(len(eightballAnswers))]
 	}
 	sender.Location().SendText(text)
 }
