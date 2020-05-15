2ee8ed5fd9a2f5fd69056a4f1a93686695a7f8c0
TheDiscordian <therealdiscordian@gmail.com>
2020-05-15 00:05:16 -0400

    Added formatting

--- plugins/tarkovbud.go
+++ plugins/tarkovbud.go
@@ -41,58 +41,71 @@ func (tb *TarkovBuddy) Version() string {
 // tarkovMap searches gamemaps
 func tarkovMap(msg onelib.Message, sender onelib.Sender) {
 	interchange := "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/e/e5/InterchangeMap_Updated_4.24.2020.png?version=c1114bd10889074ca8c8d85e3d1fb04b"
+	interchangeFormatted := "Map of <a href=https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/e/e5/InterchangeMap_Updated_4.24.2020.png?version=c1114bd10889074ca8c8d85e3d1fb04b>Interchange</a>."
 	reserve := "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/c/c0/ReserveMap3d.jpg?version=2b5fcc2b5f557535a42002e31c17c113"
+	reserveFormatted := "Map of <a href=https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/c/c0/ReserveMap3d.jpg?version=2b5fcc2b5f557535a42002e31c17c113>Reserve</a>."
 	woods := "https://cdn.gamerjournalist.com/primary/2020/01/Escape-From-Tarkov-Woods-Map-Guide-2020-scaled.jpg"
+	woodsFormatted := "Map of <a href=https://cdn.gamerjournalist.com/primary/2020/01/Escape-From-Tarkov-Woods-Map-Guide-2020-scaled.jpg>Woods</a>."
 	shoreline := "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/d/d4/Shoreline_marvelin_2_updated.png?version=f28651df0d566bdc1996aeeacb496d15"
+	shorelineFormatted := "Map of <a href=https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/d/d4/Shoreline_marvelin_2_updated.png?version=f28651df0d566bdc1996aeeacb496d15>Shoreline</a>."
 	labs := "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/0/0b/TheLabMapFull.png?version=8743e690fbd315e114f51540347eca29"
+	labsFormatted := "Map of <a href=https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/0/0b/TheLabMapFull.png?version=8743e690fbd315e114f51540347eca29>Labs</a>."
 	factory := "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/8/83/Factory_0.8.png?version=91f04c0c3f62388c86e3fbebdd0abcdf"
+	factoryFormatted := "Map of <a href=https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/8/83/Factory_0.8.png?version=91f04c0c3f62388c86e3fbebdd0abcdf>Factory</a>."
 	customs := "https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/c/c8/Customs_Nuxx_20190106_1.2.png?version=a3b44edf49616eaad2736c6523c977b0"
-	maphelp := "https://escapefromtarkov.gamepedia.com/Map_of_Tarkov"
+	customsFormatted := "Map of <a href=https://gamepedia.cursecdn.com/escapefromtarkov_gamepedia/c/c8/Customs_Nuxx_20190106_1.2.png?version=a3b44edf49616eaad2736c6523c977b0>Customs</a>."
+	maphelp := "Sorry, try this: https://escapefromtarkov.gamepedia.com/Map_of_Tarkov"
+	maphelpFormatted := "Sorry, try this: <a href=https://escapefromtarkov.gamepedia.com/Map_of_Tarkov>Map of Tarkov</a>."
 
 	if txt := msg.Text(); txt == "interchange" {
-		sender.Location().SendText(interchange)
+		sender.Location().SendFormattedText(interchange, interchangeFormatted)
 	} else if txt == "reserve" {
-		sender.Location().SendText(reserve)
+		sender.Location().SendFormattedText(reserve, reserveFormatted)
 	} else if txt == "woods" {
-		sender.Location().SendText(woods)
+		sender.Location().SendFormattedText(woods, woodsFormatted)
 	} else if txt == "shoreline" {
-		sender.Location().SendText(shoreline)
+		sender.Location().SendFormattedText(shoreline, shorelineFormatted)
 	} else if txt == "labs" {
-		sender.Location().SendText(labs)
+		sender.Location().SendFormattedText(labs, labsFormatted)
 	} else if txt == "factory" {
-		sender.Location().SendText(factory)
+		sender.Location().SendFormattedText(factory, factoryFormatted)
 	} else if txt == "customs" {
-		sender.Location().SendText(customs)
+		sender.Location().SendFormattedText(customs, customsFormatted)
 	} else {
-		sender.Location().SendText("Sorry, try this: " + maphelp)
+		sender.Location().SendFormattedText(maphelp, maphelpFormatted)
 	}
 }
 
 func tarkovBoss(msg onelib.Message, sender onelib.Sender) {
 	// find boss searching by map
-	customs := "https://escapefromtarkov.gamepedia.com/Reshala"
-	reserve := "https://escapefromtarkov.gamepedia.com/Glukar"
-	interchange := "https://escapefromtarkov.gamepedia.com/Killa"
-	woods := "https://escapefromtarkov.gamepedia.com/Shturman"
+	customs := "https://escapefromtarkov.gamepedia.com/Reshala is the boss of Customs."
+	customsFormatted := "<a href=https://escapefromtarkov.gamepedia.com/Reshala>Reshala</a> is the boss of Customs."
+	reserve := "https://escapefromtarkov.gamepedia.com/Glukar is the boss of Reserve."
+	reserveFormatted := "<a href=https://escapefromtarkov.gamepedia.com/Glukar>Glukar</a> is the boss of Reserve."
+	interchange := "https://escapefromtarkov.gamepedia.com/Killa is the boss of Interchange."
+	interchangeFormatted := "<a href=https://escapefromtarkov.gamepedia.com/Killa>Killa</a> is the boss of Interchange."
+	woods := "https://escapefromtarkov.gamepedia.com/Shturman is the boss of Woods."
+	woodsFormatted := "<a href=https://escapefromtarkov.gamepedia.com/Shturman>Shturman</a> is the boss of Woods."
 
-	bosshelp := "https://escapefromtarkov.gamepedia.com/Characters#Bosses"
+	bosshelp := "Sorry, try this: https://escapefromtarkov.gamepedia.com/Characters#Bosses"
+	bosshelpFormatted := "Sorry, try this: <a href=https://escapefromtarkov.gamepedia.com/Characters#Bosses>Characters#Bosses</a>"
 
 	if txt := msg.Text(); txt == "interchange" || txt == "killa" {
-		sender.Location().SendText(interchange)
+		sender.Location().SendFormattedText(interchange, interchangeFormatted)
 	} else if txt == "reserve" || txt == "glukar" {
-		sender.Location().SendText(reserve)
+		sender.Location().SendFormattedText(reserve, reserveFormatted)
 	} else if txt == "woods" || txt == "shturman" {
-		sender.Location().SendText(woods)
+		sender.Location().SendFormattedText(woods, woodsFormatted)
 	} else if txt == "customs" || txt == "reshala" {
-		sender.Location().SendText(customs)
+		sender.Location().SendFormattedText(customs, customsFormatted)
 	} else {
-		sender.Location().SendText("Sorry, try this: " + bosshelp)
+		sender.Location().SendFormattedText(bosshelp, bosshelpFormatted)
 	}
 }
 
 // Implements returns the function to call tarkovMap.
 func (tb *TarkovBuddy) Implements() (map[string]onelib.Command, *onelib.Monitor) {
-	return map[string]onelib.Command{"tarkov map": tarkovMap, "tarkov boss": tarkovBoss}, nil
+	return map[string]onelib.Command{"map": tarkovMap, "boss": tarkovBoss}, nil
 }
 
 // Remove is required.
