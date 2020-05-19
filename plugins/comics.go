c261c6a201a66be7a29e15889da9d1b2d05d0d18
TheDiscordian <therealdiscordian@gmail.com>
2020-05-19 01:02:11 -0400

    Updated comics plugin /w license & formatting.

--- plugins/comics.go
+++ plugins/comics.go
@@ -1,9 +1,14 @@
+// Copyright (c) 2020, The OneBot Contributors. All rights reserved.
+
 package main
 
 import (
 	"fmt"
 	"github.com/TheDiscordian/onebot/onelib"
+	"io/ioutil"
 	"math/rand"
+	"net/http"
+	"strings"
 	"time"
 )
 
@@ -32,12 +37,28 @@ func (cp *ComicPlugin) Version() string {
 	return VERSION
 }
 
+func getComicInfo(url string) (title string, imageURL string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	var rawText []byte
+	rawText, _ = ioutil.ReadAll(resp.Body)
+	text := string(rawText)
+	text = text[strings.Index(text, "<title>")+7:]
+	title = text[:strings.Index(text, "</title>")]
+	text = text[strings.Index(text, "Image URL (for hotlinking/embedding): ")+38:]
+	imageURL = text[:strings.Index(text, "\n")]
+	return
+}
+
 func comic(msg onelib.Message, sender onelib.Sender) {
 	min := 200
 	max := 2307 //TODO give link to main page for newest updates
-	comicn := rand.Intn(max-min+1) + min
-	text := fmt.Sprintf("Your comic: https://www.xkcd.com/%d", comicn)
-	formattedText := fmt.Sprintf("Your comic: <a href=https://www.xkcd.com/%d>here</a>", comicn)
+	url := fmt.Sprintf("https://www.xkcd.com/%d", rand.Intn(max-min+1)+min)
+	title, imageURL := getComicInfo(url)
+	text := fmt.Sprintf("Your comic: \"%s\": %s", title, url)
+	formattedText := fmt.Sprintf("Your comic: <a href=%s>%s</a> (<a href=%s>Web</a>)", imageURL, title, url)
 	sender.Location().SendFormattedText(text, formattedText)
 }
 
