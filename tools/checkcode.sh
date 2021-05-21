cf353c9bf6f1f3d490c612b27a0d8bc3a6612b95
TheDiscordian <therealdiscordian@gmail.com>
2021-05-21 14:29:14 -0400

    Upgraded checkcode.sh, now will use golangci-lint if available

--- tools/checkcode.sh
+++ tools/checkcode.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+echo "Running go vet..."
+echo
+
+echo "onebot"
 go vet github.com/TheDiscordian/onebot
+echo "onelib"
 go vet github.com/TheDiscordian/onebot/onelib
+echo "./libs/*"
 go vet ./libs/*
 for f in ./plugins/*.go; do
 	echo ${f};
@@ -11,3 +17,27 @@ for f in ./protocols/*.go; do
 	echo ${f};
 	go vet ${f};
 done;
+
+if command -v golangci-lint &> /dev/null
+then
+	echo
+	echo "Running golangci-lint..."
+	echo
+
+	EXCLUDES='\`Load\` is unused'
+
+	echo "onebot"
+	golangci-lint run -e "$EXCLUDES" *.go
+	echo "onelib"
+	golangci-lint run -e "$EXCLUDES" ./onelib
+	echo "./libs/*"
+	golangci-lint run -e "$EXCLUDES" ./libs/*
+	for f in ./plugins/*.go; do
+		echo ${f};
+		golangci-lint run -e "$EXCLUDES" ${f};
+	done;
+	for f in ./protocols/*.go; do
+		echo ${f};
+		golangci-lint run -e "$EXCLUDES" ${f};
+	done;
+fi
