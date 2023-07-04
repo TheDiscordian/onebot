ec053d0989a28cb97754aa940fcfad36ebd747fc
Discordian <therealdiscordian@gmail.com>
2023-07-04 15:07:21 -0400

    URLs are no longer stripped out from Markdown (but reformatted), new 'ingest' feature to learn bits, code broken up to make upgrading easier.

--- plugins/qa/qa.py
+++ plugins/qa/qa.py
@@ -8,33 +8,37 @@ from openai.embeddings_utils import distances_from_embeddings
 # Default model
 model = "gpt-3.5-turbo"
 
+def get_file(url, prod, filename):
+	# check if dbs/prod exists, if not, create it
+	if not os.path.exists("plugins/qa/dbs/%s" % prod):
+		os.makedirs("plugins/qa/dbs/%s" % prod)
+
+	page = urllib.request.urlopen(url)
+	text = page.read().decode("utf-8")
+	# only modify md files
+	if filename.endswith(".md"):
+		# remove html tags
+		text = re.sub(r'<[^>]*>', '', text)
+		# Transform Markdown links, preserving the link
+		text = re.sub(r"\[(.*?)\]\((.*?)\)", r"\1 (\2)", text)
+		# remove all blocks which begin with ":::callout" and end with ":::"
+		text = re.sub(r':::callout[\s\S]*?:::', '', text)
+		# remove all "**", as long as it closes "**" later
+		text = re.sub(r'\*\*([^\*]+)\*\*', r'\1', text)
+		# remove all square bracket enclosures
+		text = re.sub(r'\[([^\]]+)\]', r'\1', text)
+
+	open("plugins/qa/dbs/%s/%s" % (prod, filename), "w").write(text.replace("\n\n\n", "\n\n"))
+
 def update_databases():
 	for prod in EXPERTISE:
 		print("-- Updating %s --" % prod)
-		# check if dbs/prod exists, if not, create it
-		if not os.path.exists("plugins/qa/dbs/%s" % prod):
-			os.makedirs("plugins/qa/dbs/%s" % prod)
 		# download all the documents contained in EXPERTISE[prod] and save them in dbs/prod
 		for url in EXPERTISE[prod]:
 			filename = url.split("/")[-1]
 			print("Downloading %s..." % filename)
 			try:
-				page = urllib.request.urlopen(url)
-				text = page.read().decode("utf-8")
-				# only modify md files
-				if filename.endswith(".md"):
-					# remove html tags
-					text = re.sub(r'<[^>]*>', '', text)
-					# remove markdown links, preserving the link name
-					text = re.sub(r'\[([^\]]+)\]\([^\)]+\)', r'\1', text)
-					# remove all blocks which begin with ":::callout" and end with ":::"
-					text = re.sub(r':::callout[\s\S]*?:::', '', text)
-					# remove all "**", as long as it closes "**" later
-					text = re.sub(r'\*\*([^\*]+)\*\*', r'\1', text)
-					# remove all square bracket enclosures
-					text = re.sub(r'\[([^\]]+)\]', r'\1', text)
-
-				open("plugins/qa/dbs/%s/%s" % (prod, filename), "w").write(text.replace("\n\n\n", "\n\n")) # Does the replace here really help?
+				get_file(url, prod, filename)
 			except Exception as e:
 				print(e)
 				time.sleep(1)
@@ -44,58 +48,54 @@ def update_databases():
 	# reload_database()
 	print()
 
-def db_to_aidb(pricing=False):
+def get_file_texts(tech_file):
 	ai_tech_texts = []
-	ai_tech_files = []
-	# list all the files in dbs, including in subdirectories, store list in ai_tech_texts
-	for root, dirs, files in os.walk("plugins/qa/dbs"):
-		for file in files:
-			ai_tech_files.append(os.path.join(root, file))
-	for tech_file in ai_tech_files:
-		# read the file
-		text = open(tech_file, "r").read()
+	# read the file
+	text = open(tech_file, "r").read()
 
-		title = ""
-		last_sub_title = ""
-		texts = []
-		if tech_file.endswith(".md"):
-			# find out if one of the first lines is a title
-			textlines = text.split("\n")
-			last_split = 0
-			for l in range(len(textlines)):
-				if textlines[l].startswith("# "):
-					title = textlines[l][2:]
-				if textlines[l].startswith("title: "):
-					title = textlines[l][7:]
-				# locate lines with subheadings, and break them up into more texts, stored in variable "texts"
-				if textlines[l].startswith("## "):
-					last_sub_title = title + " - " + textlines[l][3:]
-					texts.append((last_sub_title, '\n'.join(textlines[last_split:l])))
-					last_split = l
-				if textlines[l].startswith("### "):
-					if last_sub_title == "":
-						last_sub_title = title
-					texts.append((last_sub_title + " - " + textlines[l][4:], '\n'.join(textlines[last_split:l])))
-					last_split = l
+	title = ""
+	last_sub_title = ""
+	texts = []
+	if tech_file.endswith(".md"):
+		# find out if one of the first lines is a title
+		textlines = text.split("\n")
+		last_split = 0
+		for l in range(len(textlines)):
+			if textlines[l].startswith("# "):
+				title = textlines[l][2:]
+			if textlines[l].startswith("title: "):
+				title = textlines[l][7:]
+			# locate lines with subheadings, and break them up into more texts, stored in variable "texts"
+			if textlines[l].startswith("## "):
+				last_sub_title = title + " - " + textlines[l][3:]
+				texts.append((last_sub_title, '\n'.join(textlines[last_split:l])))
+				last_split = l
+			if textlines[l].startswith("### "):
+				if last_sub_title == "":
+					last_sub_title = title
+				texts.append((last_sub_title + " - " + textlines[l][4:], '\n'.join(textlines[last_split:l])))
+				last_split = l
 
-		if title == "":
-			# extrapolate title from filename
-			split_file = tech_file.split("/")
-			title = (split_file[-2] + " " + split_file[-1].split(".")[0].replace("-", ' ')).title()
-		
-		if len(texts) == 0:
-			#                     title, text
-			ai_tech_texts.append((title, text))
-		else:
-			for t in texts:
-				if len(t[1].strip()) > 25:
-					ai_tech_texts.append((t[0], t[1]))
+	if title == "":
+		# extrapolate title from filename
+		split_file = tech_file.split("/")
+		title = (split_file[-2] + " " + split_file[-1].split(".")[0].replace("-", ' ')).title()
+	
+	if len(texts) == 0:
+		#                     title, text
+		ai_tech_texts.append((title, text))
+	else:
+		for t in texts:
+			if len(t[1].strip()) > 25:
+				ai_tech_texts.append((t[0], t[1]))
 
+	return ai_tech_texts
+
+def get_embeddings(ai_tech_texts, pricing=False, append=False):
 	df = pd.DataFrame(ai_tech_texts, columns = ['title', 'text'])
 	df.head()
 
 	tokenizer = tiktoken.get_encoding("cl100k_base")
-	# df = pd.read_csv('processed/scraped.csv', index_col=0)
 	df.columns = ['title', 'text']
 	global ntokens
 	ntokens = 0
@@ -112,7 +112,10 @@ def db_to_aidb(pricing=False):
 		print("Tokens used: %d ($%.2f to process)" % (ntokens, ntokens / 1000 * 0.0004))
 		inp = input("Continue? (y/N) ")
 	if inp.lower() != "y":
-		df.to_csv(DB, index=False, encoding='utf-8')
+		if append:
+			df.to_csv(DB, index=False, encoding='utf-8', mode='a', header=False)
+		else:
+			df.to_csv(DB, index=False, encoding='utf-8')
 		return
 
 	# print("... Processing embeds do NOT stop this process for any reason! ...")
@@ -127,8 +130,23 @@ def db_to_aidb(pricing=False):
 
 	df['embeddings'] = df.text.apply(process_embeds)
 	socket.setdefaulttimeout(10)
-	df.to_csv(EMBED_DB, index=False, encoding='utf-8')
+	if append:
+		df.to_csv(EMBED_DB, index=False, encoding='utf-8', mode='a', header=False)
+	else:
+		df.to_csv(EMBED_DB, index=False, encoding='utf-8')
 	df.head()
+
+def db_to_aidb(pricing=False):
+	ai_tech_texts = []
+	ai_tech_files = []
+	# list all the files in dbs, including in subdirectories, store list in ai_tech_texts
+	for root, dirs, files in os.walk("plugins/qa/dbs"):
+		for file in files:
+			ai_tech_files.append(os.path.join(root, file))
+	for tech_file in ai_tech_files:
+		ai_tech_texts += get_file_texts(tech_file)
+
+	get_embeddings(ai_tech_texts, pricing=pricing)
 
 def create_context(question, max_len=1100, max_count=6, size="ada"):
 	"""
@@ -157,11 +175,11 @@ def create_context(question, max_len=1100, max_count=6, size="ada"):
 
 		# Add the length of the text to the current length
 		cur_len += row['n_tokens'] + 4
-		
+
 		# If the context is too long, break
 		if cur_len > max_len:
 			break
-		
+
 		# Else add it to the text that is being returned
 		#returns.append("Name: %s\nDescription: %s" % (i, row["text"]))
 		returns.append(row["text"])
@@ -259,16 +277,19 @@ cheap_tokens_used = 0
 # Run with: python3 pln_qa.py --help
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Ask a question about anything. To use AI functions, set your OpenAPI key via the environment variable "OPENAI_API_KEY".')
-	parser.add_argument('action', type=str, help='The action to take (ex: db, aidb, question).')
+	parser.add_argument('action', type=str, help='The action to take (ex: db, aidb, question, ingest).')
 	parser.add_argument('--question', '-q', type=str, help='The question to ask.')
 	parser.add_argument('--model', '-m', type=str, help='The model to use (default: %s).' % model)
 	parser.add_argument('--debug', '-d', type=bool, help='Debug mode (print more info).')
 	parser.add_argument('--pricing', type=bool, help='Show pricing info.')
-	parser.add_argument('--expertise', '-e', type=str, help='Path to json file containing expertise.')
+	parser.add_argument('--expertise', '-e', type=str, help='Path to json file containing expertise (used with "db" action)".')
 	parser.add_argument('--prompt', '-p', type=str, help='Prompt to use (default: %s).' % PROMPT)
-	parser.add_argument('--misinfos', '-mi', type=str, help='Path to json file containing misinfo.')
+	parser.add_argument('--misinfos', '-mi', type=str, help='Path to json file containing misinfo (used with "question" action)".')
 	parser.add_argument('--database', '-db', type=str, help='Path to csv file containing database (default: %s).' % DB)
 	parser.add_argument('--embeddb', '-edb', type=str, help='Path to csv file containing database with embeddings (default: %s).' % EMBED_DB)
+	parser.add_argument('--url', '-u', type=str, help='URL to use for "ingest" action.')
+	parser.add_argument('--subject', type=str, help='Subject to use for "ingest" action (Ex: "ipfs").')
+	parser.add_argument('--filename', '-f', type=str, help='Filename to use for "ingest" action (optional).')
 	args = parser.parse_args()
 	if args.action is None:
 		# Print help text, and exit
@@ -336,6 +357,30 @@ if __name__ == "__main__":
 			exit()
 
 		db_to_aidb(pricing=pricing)
+	elif action == "ingest":
+		# Get OpenAI API Key from env var
+		if 'OPENAI_API_KEY' in os.environ:
+			OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+			openai.api_key = OPENAI_API_KEY
+		else:
+			print("No OPENAI_API_KEY environment variable found, exiting...")
+			exit()
+		if args.url is None:
+			print("Specify a url with '-u <url>'")
+			parser.print_help()
+			exit(1)
+		if args.subject is None:
+			print("Specify a subject with '--subject <subject>'")
+			parser.print_help()
+			exit(1)
+		filename = ""
+		if args.filename is not None:
+			filename = args.filename
+		if filename == "":
+			filename = args.url.split("/")[-1]
+		get_file(args.url, args.subject, filename)
+		texts = get_file_texts("plugins/qa/dbs/%s/%s" % (args.subject, filename))
+		get_embeddings(texts, pricing=pricing, append=True)
 	else:
 		parser.print_help()
 		exit(1)
